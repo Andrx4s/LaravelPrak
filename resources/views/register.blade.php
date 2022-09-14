acc9a06ea10790beb57f17958637109eec9d951b
is41-munirovaa <123>
2022-09-14 13:56:00 +0500

    Version 1.0.1

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -1,30 +1,69 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Страница регистрации</title>
-</head>
-<body>
-<form action="{{route('register')}}" method="POST">
-    @if(session()->has('success'))
-        <h3>Операция регистрации успешно выполнена</h3>
-    @endif
-    @csrf
-        <input type="text" name="fullname" placeholder="Ваше ФИО"><br>
-        @error('fullname')<p>{{$message}}</p>@enderror
-        <input type="text" name="login" placeholder="Ваш логин"><br>
-        @error('login')<p>{{$message}}</p>@enderror
-        <input type="number" name="age" placeholder="Дата рождения"><br>
-        @error('age')<p>{{$message}}</p>@enderror
-        <input type="text" name="address" placeholder="Ваш адрес"><br>
-        @error('address')<p>{{$message}}</p>@enderror
-        <input type="password" name="password" placeholder="Ваш пароль"><br>
-        @error('password')<p>{{$message}}</p>@enderror
-        <input type="password" name="password_confirmation" placeholder="Повторите ваш пароль"><br>
-        <input type="submit" value="Регистрация">
-</form>
-</body>
-</html>
+@extends('index')
+
+@section('title', 'Страница регистрации')
+
+@section('content')
+    <div class="container p-4">
+        <div class="row">
+            <div class="col"></div>
+            <div class="col-6">
+                <form action="{{route('register')}}" method="POST">
+                    @if(session()->has('success'))
+                        <h3>Операция регистрации успешно выполнена</h3>
+                    @endif
+                    @csrf
+                        <div class="mb-3">
+                            <label for="inputName" class="form-label">Ваше ФИО</label>
+                            <input type="email"
+                                   class="form-control @error('fullname') is-invalid @enderror"
+                                   id="inputName"
+                                   name="fullname">
+                        </div>
+                        <div class="mb-3">
+                            <label for="inputLogin" class="form-label">Ваш логин</label>
+                            <input type="text"
+                                   class="form-control @error('login') is-invalid @enderror"
+                                   id="inputLogin"
+                                   name="login">
+                        </div>
+                        <div class="mb-3">
+                            <label for="inputAge" class="form-label">Возраст</label>
+                            <input type="number"
+                                   class="form-control @error('age') is-invalid @enderror"
+                                   id="inputAge"
+                                   name="age">
+                        </div>
+                        <div class="mb-3">
+                            <label for="inputAddress" class="form-label">Ваш адрес</label>
+                            <input type="text"
+                                   class="form-control @error('address') is-invalid @enderror"
+                                   id="inputAddress"
+                                   name="address">
+                        </div>
+                        <div class="mb-3">
+                            <label for="inputPassword" class="form-label">Ваш пароль</label>
+                            <input type="password"
+                                   class="form-control @error('password') is-invalid @enderror"
+                                   id="inputPassword"
+                                   name="password">
+                        </div>
+                        <div class="mb-3">
+                            <label for="inputPasswordConfirmation" class="form-label">Повторите ваш пароль</label>
+                            <input type="password"
+                                   class="form-control @error('password_confirmation') is-invalid @enderror"
+                                   id="inputPasswordConfirmation"
+                                   name="password_confirmation"
+                                   aria-describedby="invalidPasswordConfirmation">
+                            @error('password_confirmation')
+                            <div id="invalidPasswordConfirmation" class="invalid-feedback">
+                                {{ $message }}
+                            </div>
+                            @enderror
+                        </div>
+                    <button type="submit" class="btn btn-primary">Регистрация</button>
+                </form>
+            </div>
+            <div class="col"></div>
+        </div>
+    </div>
+@endsection
